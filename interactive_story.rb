0b358519576cf292135a03a10e267658ee3e496f
goldenporsche <katiebaldesweiler@gmail.com>
2016-12-13 19:26:00 -0500

    structuring

--- interactive_story.rb
+++
@@ -1,8 +0,0 @@
-
-puts "You’re a traveler on a long journey. After many miles, you come to a fork in the road. To the North is a small village.  To the East is dark cave. Which way do you go? North or East?"
-direction = gets.chomp
-
-	if direction == "east"
-		then puts "Oh joy, you didn't die."
-	else puts "You are dead."
-	end
